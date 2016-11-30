bb0d3192a660d30e7efbcac0d802088c75b68a4f
EdoMagen <edo.w3c@gmail.com>
2016-11-30 18:20:28 +0200

    initial commit
    Create infrastructure for external development for labOS using Angular 2 and Material Design

---
+++ src/app/order/order.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+@Component({
+    templateUrl: 'order.component.html',
+    styleUrls: ['order.component.scss'],
+})
+export class OrderComponent implements OnInit, OnDestroy {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngOnDestroy() {
+
+    }
+}
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+@Component({
+    selector: 'page-not-found',
+    templateUrl: 'page-not-found.component.html',
+    styleUrls: ['page-not-found.component.scss'],
+})
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngOnDestroy() {
+
+    }
+}
