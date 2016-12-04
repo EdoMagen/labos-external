83f2c2fea8e5310523c7cf7ae4d9c7efda3ca917
Edo Magen <edo.m@stms.co.il>
2016-12-04 12:37:54 +0200

    comment removal

--- js/main.js
+++ js/main.js
@@ -1,107 +1,5 @@
-/*
-var mockOrderPayload = {
-    "stat": true,
-    "creationDate": {
-        "dateTime": "01-03-1980 08:00:00",
-        "formattedDate": "03/01/1980",
-        "formattedTime": "08:00 AM"
-    },
-    "facility": {
-        "code": 1234,
-        "name": "Abaci Faika United Kingdom"
-    },
-    "physician": {
-        "code": 1368,
-        "name": "Marquit Harold",
-        "firstName": "Marquit",
-        "lastName": "Harold",
-        "isLinked": "true",
-        "inactive": "false",
-        "defaultIdType": "string"
-    },
-    "patient": {
-        "code": 46742,
-        "firstName": "Gal",
-        "lastName": "Montgomery",
-        "middleName": "Burns",
-        "fullName": "Gal Burns Montgomery",
-        "defaultId": "1256325412",
-        "birthYear": 2013,
-        "age": "2Y"
-    },
-    "requests": [
-        {
-            "test": {
-                "code": 12048,
-                "name": "PROT,UR",
-                "longName": "Protein, Urine"
-            },
-            "parent": {
-                "test": {
-                    "code": 12048,
-                    "name": "PROT,UR",
-                    "longName": "Protein, Urine"
-                },
-                "serialTime": 30
-            },
-            "serialTime": 30,
-            "stat": false,
-            "comments": [
-                {
-                    "internalCode": 9800018,
-                    "code": 18,
-                    "text": "This requisition form replaces all previous form.",
-                    "category": 98,
-                    "type": 0,
-                    "printable": true,
-                    "entity": "PATIENT",
-                    "deletable": true
-                }
-            ],
-            "diagnosis": [
-                {
-                    "internalCode": 9800018,
-                    "code": 18,
-                    "text": "This requisition form replaces all previous form.",
-                    "category": 98,
-                    "type": 0,
-                    "printable": true,
-                    "entity": "PATIENT",
-                    "deletable": true
-                }
-            ]
-        }
-    ],
-    "comments": [
-        {
-            "internalCode": 9800018,
-            "code": 18,
-            "text": "This requisition form replaces all previous form.",
-            "category": 98,
-            "type": 0,
-            "printable": true,
-            "entity": "PATIENT",
-            "deletable": true
-        }
-    ],
-    "diagnosis": [
-        {
-            "internalCode": 9800018,
-            "code": 18,
-            "text": "This requisition form replaces all previous form.",
-            "category": 98,
-            "type": 0,
-            "printable": true,
-            "entity": "PATIENT",
-            "deletable": true
-        }
-    ]
-}
-*/
-
 // Variables / members
-// -----------------------
-var baseUrl = 'http://saaslims1:83/s/';
+var baseUrl = 'http://saaslims1:90/s/';
 var apiToken = _getParameterByName('apiToken');
 
 var orderPayload = {
@@ -160,7 +58,6 @@ function _getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-
 
 // Methods to get the items we need to populate our form
 function getPatientsList() {
@@ -255,7 +152,6 @@ function getTestsList() {
         }
     });
 }
-
 
 // Methods to send our order to be saved by BE
 function onButtonClick(event) {
